00fe9297737214afcd0dc7606df7c2072813ef3a
esmonddsouza <esmonddsouza42@gmail.com>
2020-06-16 10:08:19 +0100

    Minor changes

--- backend/crime_chain/Intel_SGX/SGX_Remote_Attestation/remote_node.py
+++ backend/crime_chain/Intel_SGX/SGX_Remote_Attestation/remote_node.py
@@ -1,15 +1,20 @@
 
-class SGX_Application():
+def remoteAttestation():
+    """
+    predicts pollution data
+    """
+    verified = True
+    key = '123456789'
+    msg0 = 'qwerty12345'
+    msg1 = 'qwerty12345'
+    msg2 = 'qwerty12345'
+    msg3 = 'qwerty12345'
+    msg4 = 'qwerty12345'
+    return verified, key, msg0, msg1, msg2, msg3, msg4
 
-    def remoteAttestation():
-        """
-        predicts pollution data
-        """
-        verified = True
-        key = '123456789'
-        msg0 = 'qwerty12345'
-        msg1 = 'qwerty12345'
-        msg2 = 'qwerty12345'
-        msg3 = 'qwerty12345'
-        msg4 = 'qwerty12345'
-        return verified, key, msg0, msg1, msg2, msg3, msg4
+
+def get_secret_key():
+
+
+def initialize_remote_attestation():
+    
